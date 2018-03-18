cb53bbb98a0db55888aa054d89773449408650e8
Marwa Elali <marwaelali@gmail.com>
2018-03-18 11:39:02 +0000

    create 'fizz' and 'buzz'

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,13 @@
 require "fizzbuzz"
-describe "fizzbuzz" do
+
+describe "fizz" do
   it "returns 'fizz' when passed 3" do
     expect(fizzbuzz(3)).to eq "fizz"
+  end
+end
+
+describe "buzz" do
+  it "returns 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
   end
 end
