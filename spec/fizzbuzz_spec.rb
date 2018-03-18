030080e321e9a2acbe722404aa2c934650c45bfd
Marwa Elali <marwaelali@gmail.com>
2018-03-18 11:58:23 +0000

    multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,6 +6,12 @@ describe "fizz" do
   end
 end
 
+describe "fizz" do
+  it "returns 'fizz' when passed a multiple of 3" do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+end
+
 describe "buzz" do
   it "returns 'buzz' when passed 5" do
     expect(fizzbuzz(5)).to eq "buzz"
